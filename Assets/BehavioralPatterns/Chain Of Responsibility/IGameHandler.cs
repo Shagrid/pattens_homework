1d017af29ba996784ef7d3ccc1c9a8e59a5c8b87
Shagrid <shagrid@yandex.ru>
2020-05-03 18:21:47 +0300

    Загрузка проекта

---
+++ Assets/BehavioralPatterns/Chain Of Responsibility/IGameHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace ChainOfResponsibility
+{
+    public interface IGameHandler
+    {
+        void Handle(); //вернуть пришедший объект с некоторыми модификациями Handle(прихордит объект);
+        IGameHandler SetNext(IGameHandler nextHandler);
+    }
+}
